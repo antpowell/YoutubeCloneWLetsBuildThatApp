9787523ecaa4db234f1ef2b2b6b3801d4aa85059
antpowell <powell.anthony.e@gmail.com>
2018-01-10 15:04:50 -0600

    created a list of strings to use as video titles for recyclerView

--- app/src/main/java/com/anthony_powell/youtubeclonewletsbuildthatapp/MainAdapter.kt
+++ app/src/main/java/com/anthony_powell/youtubeclonewletsbuildthatapp/MainAdapter.kt
@@ -4,11 +4,15 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
 
 /**
  * Created by anthonypowell on 1/10/18.
  */
 class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
+
+    var videoTitles = listOf<String>("First", "Second", "Third", "Repeat", "Again", "Try", "And", "Hold")
+
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CustomViewHolder {
         var layoutInflater = LayoutInflater.from(parent?.context)
         var cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
@@ -16,15 +20,17 @@ class MainAdapter: RecyclerView.Adapter<CustomViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder?, position: Int) {
+        val videoTitle = videoTitles.get(position)
+        holder?.v?.textView_video_title?.text = videoTitle
     }
 
 
     override fun getItemCount(): Int {
-        return 15
+        return videoTitles.count()
     }
 
 }
 
-class CustomViewHolder(v: View): RecyclerView.ViewHolder(v){
+class CustomViewHolder(var v: View): RecyclerView.ViewHolder(v){
 
 }
